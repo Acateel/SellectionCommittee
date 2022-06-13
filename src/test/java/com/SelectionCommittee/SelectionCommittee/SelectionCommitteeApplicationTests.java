92ac13c75383d695d4b12a09cd5c6f34cc43dbde
Taras Kozodoi <acateely@gmail.com>
2022-06-13 21:15:14 +0300

    first commit

---
+++ src/test/java/com/SelectionCommittee/SelectionCommittee/SelectionCommitteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SelectionCommittee.SelectionCommittee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectionCommitteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
