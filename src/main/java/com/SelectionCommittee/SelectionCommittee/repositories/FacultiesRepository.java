5f75ccedeca69ed3c930679c37a20f53b45de143
Taras Kozodoi <acateely@gmail.com>
2022-06-14 21:47:50 +0300

    add testing faculties repository

---
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/repositories/FacultiesRepository.java
@@ -0,0 +1,7 @@
+package com.SelectionCommittee.SelectionCommittee.repositories;
+
+import com.SelectionCommittee.SelectionCommittee.models.FacultiesEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FacultiesRepository extends CrudRepository<FacultiesEntity, Long> {
+}
