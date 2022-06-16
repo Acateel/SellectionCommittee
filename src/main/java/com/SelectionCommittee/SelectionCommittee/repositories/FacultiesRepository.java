22631eda41e5bcfa7f8c8b4cdcd93ff5eb86b5dc
Taras Kozodoi <acateely@gmail.com>
2022-06-16 13:17:15 +0300

    add order by methods

--- src/main/java/com/SelectionCommittee/SelectionCommittee/repositories/FacultiesRepository.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/repositories/FacultiesRepository.java
@@ -3,5 +3,11 @@ package com.SelectionCommittee.SelectionCommittee.repositories;
 import com.SelectionCommittee.SelectionCommittee.models.FacultiesEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface FacultiesRepository extends CrudRepository<FacultiesEntity, Long> {
+    List<FacultiesEntity> findAllByOrderByFacultyNameAsc();
+    List<FacultiesEntity> findAllByOrderByFacultyNameDesc();
+    List<FacultiesEntity> findAllByOrderByBudgetSeatsDesc();
+    List<FacultiesEntity> findAllByOrderByTotalSeatsDesc();
 }
