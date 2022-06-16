798c332a07fd4e49edb7e099477e366ad8392117
Taras Kozodoi <acateely@gmail.com>
2022-06-16 13:35:07 +0300

    delete post method and change get method

--- src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/FacultiesController.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/FacultiesController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -17,14 +16,7 @@ public class FacultiesController {
     private static final String FACULTIES_ATTRIBUTE_NAME = "faculties";
 
     @GetMapping("/faculties")
-    public String showFaculties(Model model) {
-        Iterable<FacultiesEntity> faculties = facultiesRepository.findAll();
-        model.addAttribute(FACULTIES_ATTRIBUTE_NAME, faculties);
-        return FACULTY_PAGE;
-    }
-
-    @PostMapping("/faculties")
-    public String showFacultiesByOrder(@RequestParam String order, Model model){
+    public String showFacultiesByOrder(@RequestParam(required = false, defaultValue = "byId") String order, Model model){
         Iterable<FacultiesEntity> faculties = getFacultiesByOrder(order);
         model.addAttribute(FACULTIES_ATTRIBUTE_NAME, faculties);
         return FACULTY_PAGE;
