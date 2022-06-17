4df60ed49b6db404f38393dcf4d111e4d43fcee0
Taras Kozodoi <acateely@gmail.com>
2022-06-17 20:31:56 +0300

    add RequestRepository

---
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/repositories/RequestRepository.java
@@ -0,0 +1,11 @@
+package com.SelectionCommittee.SelectionCommittee.repositories;
+
+import com.SelectionCommittee.SelectionCommittee.models.RequestEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
+public interface RequestRepository extends PagingAndSortingRepository<RequestEntity, Long> {
+    List<RequestEntity> findAllByFacultiesIdOrderByRatingScoreDesc(int facultiesId, Pageable pageable);
+}
