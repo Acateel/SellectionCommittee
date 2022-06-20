9b4a93a8a21a22f159a0b6eb77b8e53f91f81743
Taras Kozodoi <acateely@gmail.com>
2022-06-20 22:04:39 +0300

    add user info validation and error message

--- src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/auth_controlers/RegistrationController.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/auth_controlers/RegistrationController.java
@@ -1,12 +1,17 @@
 package com.SelectionCommittee.SelectionCommittee.controllers.auth_controlers;
 
+import com.SelectionCommittee.SelectionCommittee.auth.MyUserDetailsService;
 import com.SelectionCommittee.SelectionCommittee.models.ApplicantEntity;
 import com.SelectionCommittee.SelectionCommittee.models.UserEntity;
+import com.SelectionCommittee.SelectionCommittee.validators.Validator;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Objects;
 
 @Controller
 public class RegistrationController {
@@ -34,9 +39,15 @@ public class RegistrationController {
         model.addAttribute("user", user);
         model.addAttribute("psw_repeat", passwordRepeat);
 
+
+
         ApplicantEntity applicant = getApplicantEntity(email, lastname, firstname, surname, city, region, education);
         model.addAttribute("applicant", applicant);
 
+        boolean userChecked = checkUser(user, passwordRepeat, model);
+        if(userChecked){
+            return "auth/login";
+        }
         return "auth/register";
     }
 
@@ -58,6 +69,37 @@ public class RegistrationController {
         user.setPassword(password);
         user.setRole("applicant");
         return user;
+    }
+
+    private boolean checkUser(UserEntity user, String passwordRepeat, Model model){
+        boolean emailChecked = checkEmail(user, model);
+        boolean passwordChecked = checkPassword(user, model);
+        boolean passwordRepeatChecked = checkPasswordRepeat(user, passwordRepeat, model);
+        return emailChecked && passwordChecked && passwordRepeatChecked;
+    }
+
+    private boolean checkEmail(UserEntity user, Model model) {
+        if(!Validator.checkEmail(user.getLogin())){
+            model.addAttribute("email_error", true);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkPassword(UserEntity user, Model model){
+        if(!Validator.checkPassword(user.getPassword())){
+            model.addAttribute("password_error", true);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkPasswordRepeat(UserEntity user, String passwordRepeat, Model model){
+        if(!Objects.equals(user.getPassword(), passwordRepeat)){
+            model.addAttribute("password_repeat_error", true);
+            return false;
+        }
+        return true;
     }
 
 }
