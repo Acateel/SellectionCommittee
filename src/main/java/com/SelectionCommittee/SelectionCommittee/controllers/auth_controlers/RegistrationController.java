91bf9f5c5eb0760af369c625e4b527e55f36ce02
Taras Kozodoi <acateely@gmail.com>
2022-06-20 21:19:55 +0300

    add saving register info

--- src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/auth_controlers/RegistrationController.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/auth_controlers/RegistrationController.java
@@ -1,14 +1,63 @@
 package com.SelectionCommittee.SelectionCommittee.controllers.auth_controlers;
 
+import com.SelectionCommittee.SelectionCommittee.models.ApplicantEntity;
+import com.SelectionCommittee.SelectionCommittee.models.UserEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RegistrationController {
 
     @GetMapping("/register")
-    public String getRegistrationForm(Model model){
+    public String getRegistrationForm(Model model) {
+        model.addAttribute("user", new UserEntity());
+        model.addAttribute("applicant", new ApplicantEntity());
         return "auth/register";
     }
+
+    @PostMapping("/register")
+    public String registration(@RequestParam(name = "email") String email,
+                               @RequestParam(name = "psw") String password,
+                               @RequestParam(name = "psw-repeat") String passwordRepeat,
+                               @RequestParam(name = "lastname") String lastname,
+                               @RequestParam(name = "firstname") String firstname,
+                               @RequestParam(name = "surname") String surname,
+                               @RequestParam(name = "city") String city,
+                               @RequestParam(name = "region") String region,
+                               @RequestParam(name = "education") String education,
+                               Model model) {
+
+        UserEntity user = getUserEntity(email, password);
+        model.addAttribute("user", user);
+        model.addAttribute("psw_repeat", passwordRepeat);
+
+        ApplicantEntity applicant = getApplicantEntity(email, lastname, firstname, surname, city, region, education);
+        model.addAttribute("applicant", applicant);
+
+        return "auth/register";
+    }
+
+    private ApplicantEntity getApplicantEntity(String email, String lastname, String firstname, String surname, String city, String region, String education) {
+        ApplicantEntity applicant = new ApplicantEntity();
+        applicant.setEmail(email);
+        applicant.setLastName(lastname);
+        applicant.setName(firstname);
+        applicant.setSurname(surname);
+        applicant.setCity(city);
+        applicant.setRegion(region);
+        applicant.setNameEducationalInstitution(education);
+        return applicant;
+    }
+
+    private UserEntity getUserEntity(String email, String password) {
+        UserEntity user = new UserEntity();
+        user.setLogin(email);
+        user.setPassword(password);
+        user.setRole("applicant");
+        return user;
+    }
+
 }
