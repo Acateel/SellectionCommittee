b366687981a980405e5ff7a9c4cc6eaee74ba494
Taras Kozodoi <acateely@gmail.com>
2022-06-20 22:24:47 +0300

    add Applicant Validation

--- src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/auth_controlers/RegistrationController.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/auth_controlers/RegistrationController.java
@@ -3,6 +3,7 @@ package com.SelectionCommittee.SelectionCommittee.controllers.auth_controlers;
 import com.SelectionCommittee.SelectionCommittee.auth.MyUserDetailsService;
 import com.SelectionCommittee.SelectionCommittee.models.ApplicantEntity;
 import com.SelectionCommittee.SelectionCommittee.models.UserEntity;
+import com.SelectionCommittee.SelectionCommittee.validators.ApplicantValidator;
 import com.SelectionCommittee.SelectionCommittee.validators.UserValidator;
 import com.SelectionCommittee.SelectionCommittee.validators.Validator;
 import org.apache.catalina.User;
@@ -40,13 +41,13 @@ public class RegistrationController {
         model.addAttribute("user", user);
         model.addAttribute("psw_repeat", passwordRepeat);
 
-
-
         ApplicantEntity applicant = getApplicantEntity(email, lastname, firstname, surname, city, region, education);
         model.addAttribute("applicant", applicant);
 
         boolean userChecked = UserValidator.checkUser(user, passwordRepeat, model);
-        if(userChecked){
+        boolean applicantChecked = ApplicantValidator.checkApplicant(applicant, model);
+        if(userChecked && applicantChecked){
+            // add user and applicant to DB
             return "auth/login";
         }
         return "auth/register";
