5fb3e8b6854769dad399b41680104388d6f01e3e
Taras Kozodoi <acateely@gmail.com>
2022-06-20 22:57:13 +0300

    add ApplicantRepository

---
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/repositories/ApplicantRepository.java
@@ -0,0 +1,8 @@
+package com.SelectionCommittee.SelectionCommittee.repositories;
+
+import com.SelectionCommittee.SelectionCommittee.models.ApplicantEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ApplicantRepository extends CrudRepository<ApplicantEntity, Long> {
+    ApplicantEntity findByLastNameAndNameAndSurname(String lastname, String name, String surname);
+}
