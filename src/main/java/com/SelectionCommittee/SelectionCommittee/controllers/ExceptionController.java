e8215146afdcd05550ee360cce9690e8f86dbb27
Taras Kozodoi <acateely@gmail.com>
2022-06-23 10:54:07 +0300

    change error page

--- src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/ExceptionController.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/ExceptionController.java
@@ -3,8 +3,13 @@ package com.SelectionCommittee.SelectionCommittee.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ExceptionController {
@@ -12,9 +17,9 @@ public class ExceptionController {
     private static final String ERROR_PAGE = "error";
 
     @GetMapping("/error")
-    @ResponseStatus(value = HttpStatus.FORBIDDEN)
-    public String forbidden(Model model){
-        model.addAttribute("forbidden", true);
+    public String showError(Model model, HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        model.addAttribute("status", status);
         return ERROR_PAGE;
     }
 }
