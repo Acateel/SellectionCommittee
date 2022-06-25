7f7f19c898835264d9356f658de2d4be6a4cade5
Taras Kozodoi <acateely@gmail.com>
2022-06-25 22:54:24 +0300

    add logging in main and faculties controllers

--- src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/FacultiesController.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/FacultiesController.java
@@ -2,6 +2,7 @@ package com.SelectionCommittee.SelectionCommittee.controllers;
 
 import com.SelectionCommittee.SelectionCommittee.models.FacultiesEntity;
 import com.SelectionCommittee.SelectionCommittee.repositories.FacultiesRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@Log4j2
 public class FacultiesController {
     @Autowired
     protected FacultiesRepository facultiesRepository;
@@ -17,12 +19,14 @@ public class FacultiesController {
 
     @GetMapping("/faculties")
     public String showFacultiesByOrder(@RequestParam(required = false, defaultValue = "byId") String order, Model model){
+        log.info("Show faculties");
         Iterable<FacultiesEntity> faculties = getFacultiesByOrder(order);
         model.addAttribute(FACULTIES_ATTRIBUTE_NAME, faculties);
         return FACULTY_PAGE;
     }
 
     private Iterable<FacultiesEntity> getFacultiesByOrder(String order) {
+        log.info("Select order : {}", order);
         return switch (order) {
             case "byName" -> facultiesRepository.findAllByOrderByFacultyNameAsc();
             case "byNameRevers" -> facultiesRepository.findAllByOrderByFacultyNameDesc();
--- src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/MainController.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/MainController.java
@@ -1,13 +1,16 @@
 package com.SelectionCommittee.SelectionCommittee.controllers;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Log4j2
 public class MainController {
     @GetMapping("/")
     public String main(Model model) {
+        log.info("Show main page");
         return "main";
     }
 }
