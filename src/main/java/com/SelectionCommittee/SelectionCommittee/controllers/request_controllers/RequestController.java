0e26f0a1561e8b5fca009ebad4555552946f8052
Taras Kozodoi <acateely@gmail.com>
2022-06-26 23:15:39 +0300

    refactor RequestController

--- src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/request_controllers/RequestController.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/request_controllers/RequestController.java
@@ -11,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.NoSuchElementException;
+
 @Controller
 @Log4j2
 public class RequestController {
@@ -23,20 +25,31 @@ public class RequestController {
     private static final int PAGE_SIZE = 7;
 
     @GetMapping("/request")
-    public String showRequest(@RequestParam(required = false, defaultValue = "0") int facultyId, @RequestParam(required = false, defaultValue = "0") int page, Model model) {
+    public String showRequest(@RequestParam(required = false, defaultValue = "0") int facultyId,
+                              @RequestParam(required = false, defaultValue = "0") int page,
+                              Model model) {
         log.info("Show requests,Input: id={}, page={}", facultyId, page);
-        if(facultyId == 0){
+        if (facultyId == 0) {
             return "redirect:/faculties";
         }
         page = getPage(facultyId, page);
         model.addAttribute("page", page);
 
-        FacultiesEntity faculty = facultiesRepository.findById((long) facultyId).get();
+        FacultiesEntity faculty = getFaculties(facultyId);
         model.addAttribute("faculty", faculty);
 
         var requests = requestRepository.findAllByFacultiesIdOrderByRatingScoreDesc(facultyId, PageRequest.of(page, PAGE_SIZE));
         model.addAttribute("requests", requests);
         return "request";
+    }
+
+    private FacultiesEntity getFaculties(int facultyId) {
+        var optional = facultiesRepository.findById((long) facultyId);
+        if (optional.isEmpty()) {
+            log.error("faculty with id={} not found", facultyId);
+            throw new NoSuchElementException("Faculty not found, id=" + facultyId);
+        }
+        return optional.get();
     }
 
     private int getPage(int facultyId, int page) {
