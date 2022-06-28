474895132ad22f4dd1f80478568142dec559cba3
Taras Kozodoi <acateely@gmail.com>
2022-06-28 14:52:11 +0300

    add upload scan

--- src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/ApplicantController.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/ApplicantController.java
@@ -1,6 +1,7 @@
 package com.SelectionCommittee.SelectionCommittee.controllers;
 
 import com.SelectionCommittee.SelectionCommittee.models.ApplicantEntity;
+import com.SelectionCommittee.SelectionCommittee.models.UserEntity;
 import com.SelectionCommittee.SelectionCommittee.repositories.ApplicantRepository;
 import com.SelectionCommittee.SelectionCommittee.repositories.UserRepository;
 import lombok.extern.log4j.Log4j2;
@@ -8,7 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.security.Principal;
 import java.util.NoSuchElementException;
 
@@ -20,13 +26,31 @@ public class ApplicantController {
     @Autowired
     UserRepository userRepository;
 
+    private static final String APPLICANT_KEY = "applicant";
+
     @GetMapping("/applicant")
     public String getUserInfo(Model model, Principal principal) {
         log.info("Show applicant info");
         long applicantId = getApplicantId(principal.getName());
         ApplicantEntity applicant = getApplicant(applicantId);
-        model.addAttribute("applicant", applicant);
-        return "applicant";
+        model.addAttribute(APPLICANT_KEY, applicant);
+        return APPLICANT_KEY;
+    }
+
+    @PostMapping("/applicant")
+    public String uploadAttestation(@RequestParam("file") MultipartFile file, Model model, Principal principal) {
+        long applicantId = getApplicantId(principal.getName());
+        ApplicantEntity applicant = getApplicant(applicantId);
+        try {
+            applicant.setAttestation(file.getBytes());
+            applicantRepository.save(applicant);
+            model.addAttribute("file_upload", true);
+        } catch (IOException exception) {
+            log.error("File not upload");
+            model.addAttribute("file_not_upload", true);
+        }
+        model.addAttribute(APPLICANT_KEY, applicant);
+        return APPLICANT_KEY;
     }
 
     private ApplicantEntity getApplicant(long applicantId) {
