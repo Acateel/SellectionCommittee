89087f68bf26c9dc8fd8d8f2a5ea217dc8518217
Taras Kozodoi <acateely@gmail.com>
2022-07-12 20:16:14 +0300

    add comments into SelectionCommitteeApplication

--- src/main/java/com/SelectionCommittee/SelectionCommittee/SelectionCommitteeApplication.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/SelectionCommitteeApplication.java
@@ -3,11 +3,17 @@ package com.SelectionCommittee.SelectionCommittee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * SelectionCommitteeApplication start application
+ */
 @SpringBootApplication
 public class SelectionCommitteeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SelectionCommitteeApplication.class, args);
-	}
+    /**
+     * Run spring application
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SelectionCommitteeApplication.class, args);
+    }
 
 }
