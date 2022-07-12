fa76b5b6975277a8b82e188e9f957fe79f1b67c8
Taras Kozodoi <acateely@gmail.com>
2022-07-12 18:06:27 +0300

    add comments into AdminMenuController

--- src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/admin/AdminMenuController.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/controllers/admin/AdminMenuController.java
@@ -5,11 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
- * Admin menu controller - show admin menu with admin function
+ * Admin menu controller is responsible for displaying the admin menu page
  */
 @Controller
 @Log4j2
 public class AdminMenuController {
+    /**
+     * Show admin menu
+     * @return model name of admin menu
+     */
     @GetMapping("/admin_menu")
     public String getAdminMenu(){
         log.info("Show admin menu");
