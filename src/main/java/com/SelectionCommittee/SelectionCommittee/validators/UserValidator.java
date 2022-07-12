f123dfca1138de3f72cc2ea6e7d15d6059698f75
Taras Kozodoi <acateely@gmail.com>
2022-07-12 19:49:07 +0300

    add comments into UserValidator

--- src/main/java/com/SelectionCommittee/SelectionCommittee/validators/UserValidator.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/validators/UserValidator.java
@@ -5,10 +5,21 @@ import org.springframework.ui.Model;
 
 import java.util.Objects;
 
+/**
+ * UserValidator validate user entity and add error message into model if validate is false
+ */
 public class UserValidator {
     private UserValidator() {
     }
 
+    /**
+     * validate user entity
+     *
+     * @param user           user entity
+     * @param passwordRepeat password repeat
+     * @param model          model for add attribute for message
+     * @return true if all validate complete
+     */
     public static boolean checkUser(UserEntity user, String passwordRepeat, Model model) {
         boolean emailChecked = checkEmail(user, model);
         boolean passwordChecked = checkPassword(user, model);
@@ -16,6 +27,13 @@ public class UserValidator {
         return emailChecked && passwordChecked && passwordRepeatChecked;
     }
 
+    /**
+     * Validate user email
+     *
+     * @param user  user entity
+     * @param model for add attribute with error message if validate is false
+     * @return true or false
+     */
     public static boolean checkEmail(UserEntity user, Model model) {
         if (!Validator.checkEmail(user.getLogin())) {
             model.addAttribute("email_error", true);
@@ -24,6 +42,13 @@ public class UserValidator {
         return true;
     }
 
+    /**
+     * Validate user password
+     *
+     * @param user  user entity
+     * @param model for add attribute with error message if validate is false
+     * @return true or false
+     */
     public static boolean checkPassword(UserEntity user, Model model) {
         if (!Validator.checkPassword(user.getPassword())) {
             model.addAttribute("password_error", true);
@@ -32,6 +57,14 @@ public class UserValidator {
         return true;
     }
 
+    /**
+     * Validate user repeat password
+     *
+     * @param user           user entity
+     * @param passwordRepeat password repeat
+     * @param model          for add attribute with error message if validate is false
+     * @return true or false
+     */
     public static boolean checkPasswordRepeat(UserEntity user, String passwordRepeat, Model model) {
         if (!Objects.equals(user.getPassword(), passwordRepeat)) {
             model.addAttribute("password_repeat_error", true);
