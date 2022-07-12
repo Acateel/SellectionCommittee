668454d4d969ce5877454e1b57f8b9213db7f715
Taras Kozodoi <acateely@gmail.com>
2022-07-12 10:28:07 +0300

    refactor MyUserDetailsService (remove warning) and add comments

--- src/main/java/com/SelectionCommittee/SelectionCommittee/auth/MyUserDetailsService.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/auth/MyUserDetailsService.java
@@ -10,15 +10,27 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+/**
+ * MyUserDetailsService is responsible for returning the user by login(username) from database
+ */
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
     @Autowired
     UserRepository userRepository;
+
+    /**
+     * Load user by username from database
+     * @param username get from login form
+     * @return userDerails Object MyUserDerails for WebSecurity
+     * @throws UsernameNotFoundException when user with username not found in DB
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<UserEntity> user = userRepository.findByLogin(username);
-        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + username));
-        return user.map(MyUserDetails::new).get();
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("Not found: " + username);
+        }
+        return new MyUserDetails(user.get());
     }
 }
