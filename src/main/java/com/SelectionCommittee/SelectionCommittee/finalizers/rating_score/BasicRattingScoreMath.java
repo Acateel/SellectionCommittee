f934a94515e256a0db907849eb43dedbebd8fa3f
Taras Kozodoi <acateely@gmail.com>
2022-07-12 19:37:00 +0300

    add comments into RatingScoreMathable and BasicRattingScoreMath

--- src/main/java/com/SelectionCommittee/SelectionCommittee/finalizers/rating_score/BasicRattingScoreMath.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/finalizers/rating_score/BasicRattingScoreMath.java
@@ -2,15 +2,23 @@ package com.SelectionCommittee.SelectionCommittee.finalizers.rating_score;
 
 import com.SelectionCommittee.SelectionCommittee.models.RequestEntity;
 
+/**
+ * BasicRattingScoreMath basic setter of ratting score
+ */
+public class BasicRattingScoreMath implements RatingScoreMathable {
 
-public class BasicRattingScoreMath implements  RatingScoreMathable{
-
+    /**
+     * Set ratting score
+     *
+     * @param request applicant request
+     * @return ratting score
+     */
     @Override
     public int setRattingScore(RequestEntity request) {
         double main = request.getMainSubject();
         double second = request.getSecondSubject();
         double sub = request.getSubSubject();
-        double average = (main+second+sub)/3.0;
+        double average = (main + second + sub) / 3.0;
         return (int) Math.round(average);
     }
 }
--- src/main/java/com/SelectionCommittee/SelectionCommittee/finalizers/rating_score/RatingScoreMathable.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/finalizers/rating_score/RatingScoreMathable.java
@@ -2,6 +2,9 @@ package com.SelectionCommittee.SelectionCommittee.finalizers.rating_score;
 
 import com.SelectionCommittee.SelectionCommittee.models.RequestEntity;
 
+/**
+ * RatingScoreMathable set ratting score of request
+ */
 public interface RatingScoreMathable {
     int setRattingScore(RequestEntity request);
 }
