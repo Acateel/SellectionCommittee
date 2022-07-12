ee0a07917d4bb93e379408148095048c3f04301a
Taras Kozodoi <acateely@gmail.com>
2022-07-12 19:33:31 +0300

    add comments into PassingScoreMathable and BasicPassingScoreMath

--- src/main/java/com/SelectionCommittee/SelectionCommittee/finalizers/passing_score/BasicPassingScoreMath.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/finalizers/passing_score/BasicPassingScoreMath.java
@@ -2,8 +2,16 @@ package com.SelectionCommittee.SelectionCommittee.finalizers.passing_score;
 
 import com.SelectionCommittee.SelectionCommittee.models.RequestEntity;
 
+/**
+ * BasicPassingScoreMath basic set passing score for request
+ */
 public class BasicPassingScoreMath implements PassingScoreMathable {
 
+    /**
+     * Set passing (status allowed if ratting score more than 100)
+     *
+     * @param request applicant request
+     */
     @Override
     public void setPassing(RequestEntity request) {
         if (request.getRatingScore() < 100) {
--- src/main/java/com/SelectionCommittee/SelectionCommittee/finalizers/passing_score/PassingScoreMathable.java
+++ src/main/java/com/SelectionCommittee/SelectionCommittee/finalizers/passing_score/PassingScoreMathable.java
@@ -2,6 +2,9 @@ package com.SelectionCommittee.SelectionCommittee.finalizers.passing_score;
 
 import com.SelectionCommittee.SelectionCommittee.models.RequestEntity;
 
+/**
+ * PassingScoreMathable set passing score for request
+ */
 public interface PassingScoreMathable {
     void setPassing(RequestEntity request);
 }
